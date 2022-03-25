355e8c24c525ca426f91b6f4af8e4af69d6f3a80
ClaireMorin05 <58421535+ClaireMorin05@users.noreply.github.com>
2022-03-25 16:59:11 +0100

    tri en php

--- repertoire_botannique.php
+++ repertoire_botannique.php
@@ -36,69 +36,89 @@
                 }
             ?>
             <br/>
-            <input id="searchbar" onkeyup="recherche_plante()" type="text" placeholder="Rechercher une plante...">
+            <input id="searchbar" type="text" placeholder="Rechercher une plante..." name="plante" >
+            <button type="submit" name="searchbar">Rechercher</button>
+
 
             Type de plante :
-            <select name="liste"> 
-                <option>---</option> 
-                <option>Arbre </option>
-                <option>Arbuste </option>
-                <option>Plante </option>
-                <option>Algue </option>
+            <select name="type" onchange="this.form.submit()"> 
+                <option value="vide" <?php if($_POST['type']=="vide"){echo "selected";}?>>Tous</option> 
+                <option value="Arbre" <?php if($_POST['type']=="Arbre"){echo "selected";}?>>Arbre </option>
+                <option value="Arbuste" <?php if($_POST['type']=="Arbuste"){echo "selected";}?>>Arbuste </option>
+                <option value="Plante" <?php if($_POST['type']=="Plante"){echo "selected";}?>>Plante </option>
+                <option value="Algue" <?php if($_POST['type']=="Algue"){echo "selected";}?>>Algue </option>
+            </select>
+
+            Couleur plante : 
+            <select name="couleurPlante" onchange="this.form.submit()"> 
+                <option value="vide" <?php if($_POST['couleurPlante']=="vide"){echo "selected";}?>>Toutes</option>  
+                <option value="Jaune" <?php if($_POST['couleurPlante']=="Jaune"){echo "selected";}?>> Jaune </option> 
+                <option value="Blanc" <?php if($_POST['couleurPlante']=="Blanc"){echo "selected";}?>> Blanc </option> 
+                <option value="Rose" <?php if($_POST['couleurPlante']=="Rose"){echo "selected";}?>> Rose </option> 
+                <option value="Violet" <?php if($_POST['couleurPlante']=="Violet"){echo "selected";}?>> Violet </option> 
+                <option value="Orange" <?php if($_POST['couleurPlante']=="Orange"){echo "selected";}?>> Orange </option> 
+                <option value="Rouge" <?php if($_POST['couleurPlante']=="Rouge"){echo "selected";}?>> Rouge </option> 
+                <option value="Vert" <?php if($_POST['couleurPlante']=="Vert"){echo "selected";}?>> Vert </option> 
+                <option value="Bleu" <?php if($_POST['couleurPlante']=="Bleu"){echo "selected";}?>> Bleu </option> 
+                <option value="Marron" <?php if($_POST['couleurPlante']=="Marron"){echo "selected";}?>> Marron </option> 
+                <option value="Noir" <?php if($_POST['couleurPlante']=="Noir"){echo "selected";}?>> Noir </option> 
+                <option valeu="Gris" <?php if($_POST['couleurPlante']=="Gris"){echo "selected";}?>> Gris </option> 
             </select>
 
             Couleur fleur : 
-            <select name="listefleur">
-            <option>---</option>  
-            <option> Jaune </option> 
-            <option> Blanc </option> 
-            <option> Rose </option> 
-            <option> Violet </option> 
-            <option> Orange </option> 
-            <option> Rouge </option> 
-            <option> Vert </option> 
-            <option> Bleu </option> 
-            <option> Marron </option> 
-            <option> Noir </option> 
-            <option> Gris </option> 
-            </select>
+            <select name="couleurFleur" onchange="this.form.submit()">
+                <option value="vide" <?php if($_POST['couleurFleur']=="vide"){echo "selected";}?>>Toutes</option>
+                <option value="absence" <?php if($_POST['couleurFleur']=="absence"){echo "selected";}?>>Pas de fleur</option>    
+                <option value="Jaune" <?php if($_POST['couleurFleur']=="Jaune"){echo "selected";}?>> Jaune </option> 
+                <option value="Blanc" <?php if($_POST['couleurFleur']=="Blanc"){echo "selected";}?>> Blanc </option> 
+                <option value="Rose" <?php if($_POST['couleurFeur']=="Rose"){echo "selected";}?>> Rose </option> 
+                <option value="Violet" <?php if($_POST['couleurFleur']=="Violet"){echo "selected";}?>> Violet </option> 
+                <option value="Orange" <?php if($_POST['couleurFleur']=="Orange"){echo "selected";}?>> Orange </option> 
+                <option value="Rouge" <?php if($_POST['couleurFleur']=="Rouge"){echo "selected";}?>> Rouge </option> 
+                <option value="Vert" <?php if($_POST['couleurFleur']=="Vert"){echo "selected";}?>> Vert </option> 
+                <option value="Bleu" <?php if($_POST['couleurFleur']=="Bleu"){echo "selected";}?>> Bleu </option> 
+                <option value="Marron" <?php if($_POST['couleurFleur']=="Marron"){echo "selected";}?>> Marron </option> 
+                <option value="Noir" <?php if($_POST['couleurFleur']=="Noir"){echo "selected";}?>> Noir </option> 
+                <option valeu="Gris" <?php if($_POST['couleurFLeur']=="Gris"){echo "selected";}?>> Gris </option> 
+             </select>
 
             Couleur fruit : 
-            <select name="listefruit"> 
-            <option>---</option> 
-            <option> Jaune </option> 
-            <option> Blanc </option> 
-            <option> Rose </option> 
-            <option> Violet </option> 
-            <option> Orange </option> 
-            <option> Rouge </option> 
-            <option> Vert </option> 
-            <option> Bleu </option> 
-            <option> Marron </option> 
-            <option> Noir </option> 
-            <option> Gris </option> 
+            <select name="couleurFruit" onchange="this.form.submit()"> 
+                <option value="vide" <?php if($_POST['couleurFruit']=="vide"){echo "selected";}?>>Toutes</option> 
+                <option value="absence" <?php if($_POST['couleurFruit']=="absence"){echo "selected";}?>>Pas de fruit</option> 
+                <option value="Jaune" <?php if($_POST['couleurFruit']=="Jaune"){echo "selected";}?>> Jaune </option> 
+                <option value="Blanc" <?php if($_POST['couleurFruit']=="Blanc"){echo "selected";}?>> Blanc </option> 
+                <option value="Rose" <?php if($_POST['couleurFruit']=="Rose"){echo "selected";}?>> Rose </option> 
+                <option value="Violet" <?php if($_POST['couleurFruit']=="Violet"){echo "selected";}?>> Violet </option> 
+                <option value="Orange" <?php if($_POST['couleurFruit']=="Orange"){echo "selected";}?>> Orange </option> 
+                <option value="Rouge" <?php if($_POST['couleurFruit']=="Rouge"){echo "selected";}?>> Rouge </option> 
+                <option value="Vert" <?php if($_POST['couleurFruit']=="Vert"){echo "selected";}?>> Vert </option> 
+                <option value="Bleu" <?php if($_POST['couleurFruit']=="Bleu"){echo "selected";}?>> Bleu </option> 
+                <option value="Marron" <?php if($_POST['couleurFruit']=="Marron"){echo "selected";}?>> Marron </option> 
+                <option value="Noir" <?php if($_POST['couleurFruit']=="Noir"){echo "selected";}?>> Noir </option> 
+                <option valeu="Gris" <?php if($_POST['couleurFruit']=="Gris"){echo "selected";}?>> Gris </option> 
             </select>
 
             Période de floraison : 
-            <select name="listefruit">
-            <option>---</option>  
-            <option> Printemps </option> 
-            <option> Été </option> 
-            <option> Automne </option> 
-            <option> Hiver </option> 
+            <select name="periodeFleur" onchange="this.form.submit()">
+                <option value="vide"<?php if($_POST['periodeFleur']=="vide"){echo "selected";}?>>---</option>  
+                <option value="Printemps" <?php if($_POST['periodeFleur']=="Printemps"){echo "selected";}?>> Printemps </option> 
+                <option value="Ete" <?php if($_POST['periodeFleur']=="Ete"){echo "selected";}?>> Été </option> 
+                <option value="Automne" <?php if($_POST['periodeFleur']=="Automne"){echo "selected";}?>> Automne </option> 
+                <option value="Hiver" <?php if($_POST['periodeFleur']=="Hiver"){echo "selected";}?>> Hiver </option> 
             </select>
 
             Période de fructification : 
-            <select name="listefruit"> 
-            <option>---</option> 
-            <option> Printemps </option> 
-            <option> Été </option> 
-            <option> Automne </option> 
-            <option> Hiver </option> 
+            <select name="periodeFruit" onchange="this.form.submit()"> 
+                <option value="vide" <?php if($_POST['periodeFruit']=="vide"){echo "selected";}?>>---</option> 
+                <option value="Printemps" <?php if($_POST['periodeFruit']=="Printemps"){echo "selected";}?>> Printemps </option> 
+                <option value="Ete" <?php if($_POST['periodeFruit']=="Ete"){echo "selected";}?>> Été </option> 
+                <option value="Automne" <?php if($_POST['periodeFruit']=="Automne"){echo "selected";}?>> Automne </option> 
+                <option value="Hiver" <?php if($_POST['periodeFruit']=="Hiver"){echo "selected";}?>> Hiver </option> 
             </select>
 
 
-            <button id=tri> Trier </button>
+            <!--<button id=tri> Trier </button>-->
             
             <div class= "carre">
                 
@@ -112,28 +132,162 @@
                         die('Erreur :' . $e->getMessage());
                     }
 
-                    $requete = $BDD->prepare('SELECT * FROM plantes ORDER BY Nom_fr');
-                    $requete->execute();
-                    /* on récupère le résultt de la requête sous forme d'un tableau */
-                    $donnees = $requete->fetchAll();
-                    $lettre_courante = null;
-                    foreach ($donnees as $plante){
-                        
-                        if(($lettre_courante=='D' && $plante['Nom_fr'][0]!='D' && $plante['Nom_fr'][0]!='F') || ( $lettre_courante=='C' && $plante['Nom_fr'][0]!='D' && $plante['Nom_fr'][0]!='C') )
-                        {
-                            $premiere_lettre='E';
-                        }
-                        else
-                        {
-                            $premiere_lettre=$plante['Nom_fr'][0];
-                        }
+                    if(isset($_POST['plante']) and $_POST['plante']!="" )
+                    {
+                        $requete = $BDD->prepare('SELECT * FROM plantes WHERE Nom_fr ="'.ucfirst(strtolower($_POST['plante'])).'" OR Nom_latin ="'.ucfirst(strtolower($_POST['plante'])).'" ORDER BY Nom_fr');
+                        $requete->execute();
+                        /* on récupère le résultt de la requête sous forme d'un tableau */
+                        $donnees = $requete->fetchAll();
 
-                        if( $lettre_courante != $premiere_lettre ) {
-                            $lettre_courante=$plante['Nom_fr'][0];  
-                            echo'<div class = "lettre">';
-                            echo $premiere_lettre.".";  
-                            echo'</div>';
+                        $_POST['type']="vide";
+                        $_POST['couleurPlante']="vide";
+                        $_POST['couleurFleur']="vide";
+                        $_POST['couleurFruit']="vide";
+                        $_POST['periodeFleur']="vide";
+                        $_POST['periodeFruit']="vide";
+
+
+                    }
+
+                    else{
+
+                        $requete = $BDD->prepare('SELECT * FROM plantes ORDER BY Nom_fr');
+                        $requete->execute();
+                        /* on récupère le résultt de la requête sous forme d'un tableau */
+                        $donnees = $requete->fetchAll();
+
+                    }
+
+                    if(isset($_POST['type']) and $_POST['type']!="vide" ){
+                        
+                        foreach ($donnees as $plante){
+                            if($plante['Famille']==$_POST['type'] ){ 
+                                    $resultat[]=$plante;
+                            }
                         }
+                        $donnees=$resultat;
+                        
+                    }
+                    else{
+                        $resultat=$donnees;
+                    }
+
+                    //tri type plante
+                   if(isset($_POST['type']) and $_POST['type']!="vide" ){
+                        
+                        foreach ($donnees as $plante){
+                            if($plante['Famille']==$_POST['type'] ){ 
+                                    $resultatType[]=$plante;
+                            }
+                        }
+                        $donnees=$resultatType;   
+                    }
+                   
+                    //tri couleur plante
+                    if(isset($_POST['couleurPlante']) and $_POST['couleurPlante']!="vide" ){
+                        foreach ($donnees as $plante){
+
+                            if($plante['Couleur']==$_POST['couleurPlante'] ){ 
+                                    $resultatCouleur[]=$plante;
+                            }
+                        }
+                        $donnees=$resultatCouleur;   
+                    }
+                    
+                    //tri couleur fruit
+                    if(isset($_POST['couleurFleur'])){
+                        if($_POST['couleurFleur']!="vide" and $_POST['couleurFleur']!="absence" ){
+                            foreach ($donnees as $plante){
+
+                                if($plante['Couleur_fleur']==$_POST['couleurFleur'] ){ 
+                                        $resultatCouleurFleur[]=$plante;
+                                }
+                            }
+                            $donnees=$resultatCouleurFleur; 
+                        } 
+                        elseif($_POST['couleurFleur']=="absence"){
+                            foreach ($donnees as $plante){
+
+                                if($plante['Fleur']==0 ){ 
+                                    $resultatCouleurFleur[]=$plante;
+                                }
+                            }
+                            $donnees=$resultatCouleurFleur; 
+                        }
+                        
+                    }
+                    
+                    
+
+                    //tri couleur fruit
+                    if(isset($_POST['couleurFruit'])){ 
+                        if($_POST['couleurFruit']!="vide" and $_POST['couleurFruit']!="absence" ){
+                            foreach ($donnees as $plante){
+
+                                if($plante['Couleur_fruit']==$_POST['couleurFruit'] ){ 
+                                        $resultatCouleurFruit[]=$plante;
+                                }
+                            }
+                            $donnees=$resultatCouleurFruit;
+                        } 
+                        elseif($_POST['couleurFruit']=="absence"){
+                            foreach ($donnees as $plante){
+
+                                if($plante['Fruit']==0 ){ 
+                                    $resultatCouleurFruit[]=$plante;
+                                }
+                            }
+                            $donnees=$resultatCouleurFruit; 
+                        }   
+                    }
+                    
+                    //tri période fleur
+                    if(isset($_POST['periodeFleur']) and $_POST['periodeFleur']!="vide" ){
+                        foreach ($donnees as $plante){
+
+                            if($plante['Période_floraison']==$_POST['periodeFleur'] ){ 
+                                    $resultatPeriodeFleur[]=$plante;
+                            }
+                        }
+                        $donnees=$resultatPeriodeFleur;   
+                    }
+
+                    //tri période fruit
+                    if(isset($_POST['periodeFruit']) and $_POST['periodeFruit']!="vide" ){
+                        foreach ($donnees as $plante){
+
+                            if($plante['Période_fructification']==$_POST['periodeFruit'] ){ 
+                                    $resultatPeriodeFruit[]=$plante;
+                            }
+                        }
+                        $donnees=$resultatPeriodeFruit;   
+                    }
+
+
+                    
+                    if (count($donnees)==0)   
+                    {
+                        echo "<p align=\"center\"> Aucun résultat disponible </p>";
+                    } 
+                    else{
+                        $lettre_courante = null;
+                        foreach ($donnees as $plante){
+                            
+                            if(($lettre_courante=='D' && $plante['Nom_fr'][0]!='D' && $plante['Nom_fr'][0]!='F') || ( $lettre_courante=='C' && $plante['Nom_fr'][0]!='D' && $plante['Nom_fr'][0]!='C') )
+                            {
+                                $premiere_lettre='E';
+                            }
+                            else
+                            {
+                                $premiere_lettre=$plante['Nom_fr'][0];
+                            }
+
+                            if( $lettre_courante != $premiere_lettre ) {
+                                $lettre_courante=$plante['Nom_fr'][0];  
+                                echo'<div class = "lettre">';
+                                echo $premiere_lettre.".";  
+                                echo'</div>';
+                            }
                 ?>
                 <a href="profil_plante.php?id=<?php echo $plante['Id_plante']?> " id="lien_plante">
                     <div class = "carre_plante">
@@ -147,6 +301,7 @@
                 </a>
                 <?php
                     }
+                }
                 ?>
 
             </div>
