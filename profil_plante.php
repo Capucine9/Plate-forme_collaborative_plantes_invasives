199e10a1f7d55c9ec7a2b68f2ad9776ca06597d3
ClaireMorin05 <58421535+ClaireMorin05@users.noreply.github.com>
2022-03-26 16:35:52 +0100

    bouton connexion/deconnexion

--- profil_plante.php
+++ profil_plante.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -18,12 +21,32 @@
                     <a href="repertoire_utilisateur.php">Les utilisateurs</a>
                     <a href="listeSignalement.php">Les derniers signalements</a>
                     <a href="ajout_signalement.php">Signaler une plante</a>
-                    <a href="ajout_plante.php">Ajouter une plante</a>
+                    <?php 
+                        if($_SESSION['rang']==3){
+                            echo("<a href=\"ajout_plante.php\">Ajouter une plante</a>");
+                        }
+                    ?>
+                    
                     <a href="">Vos amis</a>
-                    <a href="connexion.php">Connexion</a>
-                    <a href="inscription.php">Inscription</a>
+                    <?php
+                        if(isset($_SESSION['id']) and empty($_GET['deco'])){
+                            echo ("<a href=\"accueil.php?deco=1\">Déconnexion</a>");
+                        }
+                        else{
+                            echo("<a href=\"connexion.php\">Connexion</a> <a href=\"inscription.php\">Inscription</a>");
+                        }
+                    ?>
+                    
+                    
                 </div>
-        </div>
+</div>
+<?php
+   if($_GET['deco']==1){
+        session_destroy();
+        echo ("<p align=\"center\"> Vous avez été déconnecté </p>");
+    }
+
+?>
 
         <form>
             
