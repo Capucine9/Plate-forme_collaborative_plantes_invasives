852827cd95e22b72eb61cf7d541b0e249d2215e3
ClaireMorin05 <58421535+ClaireMorin05@users.noreply.github.com>
2022-03-26 17:21:53 +0100

    menu

--- repertoire_utilisateur.php
+++ repertoire_utilisateur.php
@@ -1,3 +1,6 @@
+<?php
+  session_start();
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,20 +14,36 @@
 
         <!--menu déroulant-->
         <div class="deroulant">
-            <button class="menu"> Menu </button>  
-                <div class="partie" >
-                    <a href="accueil.php">Accueil</a>
-                    <a href="profil_utilisateur.php">Votre profil</a>
-                    <a href="repertoire_botannique.php">Le répertoire botannique</a>
-                    <a href="repertoire_utilisateur.php">Les utilisateurs</a>
-                    <a href="listeSignalement.php">Les derniers signalements</a>
-                    <a href="ajout_signalement.php">Signaler une plante</a>
-                    <a href="ajout_plante.php">Ajouter une plante</a>
-                    <a href="">Vos amis</a>
-                    <a href="connexion.php">Connexion</a>
-                    <a href="inscription.php">Inscription</a>
-                </div>
-        </div>
+                <button class="menu"> Menu </button>  
+                    <div class="partie" >
+                        <a href="accueil.php">Accueil</a>
+                        <?php
+                        if(isset($_SESSION['id']) and empty($_GET['deco'])){
+                            echo ("<a href=\"profil_utilisateur.php\">Votre profil</a>");
+                        }
+                        ?>
+                        <a href="repertoire_botannique.php">Le répertoire botannique</a>
+                        <a href="repertoire_utilisateur.php">Les utilisateurs</a>
+                        <a href="listeSignalement.php">Les derniers signalements</a>
+                        <a href="ajout_signalement.php">Signaler une plante</a>
+                        <?php 
+                        if($_SESSION['rang']==3){
+                            echo("<a href=\"ajout_plante.php\">Ajouter une plante</a>");
+                        }
+                        ?>
+                        <a href="">Vos amis</a>
+                        <a href="connexion.php">Connexion</a>
+                        <a href="inscription.php">Inscription</a>
+                    </div>
+    </div>
+
+    <?php
+   if($_GET['deco']==1){
+        session_destroy();
+        echo ("<p align=\"center\"> Vous avez été déconnecté </p>");
+    }
+
+  ?>
 
         
         <form action="profil_plante.php" method="POST">
