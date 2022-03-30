2d4bc982987765e52bfca23457204cfe910cba07
ClaireMorin05 <58421535+ClaireMorin05@users.noreply.github.com>
2022-03-30 10:56:26 +0200

    menu en fichier

--- ajout_signalement.php
+++ ajout_signalement.php
@@ -1,6 +1,4 @@
-<?php
-    session_start();
-?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -15,46 +13,10 @@
   <body>
     <div id="header">Plate-forme collaborative de lutte contre les plantes invasives</div>
 
-    <!--menu déroulant-->
-    <div class="deroulant">
-            <button class="menu"> Menu </button>  
-                <div class="partie" >
-                    <a href="accueil.php">Accueil</a>
-                    <?php
-                        if(isset($_SESSION['id']) and empty($_GET['deco'])){
-                            echo ("<a href=\"profil_utilisateur.php\">Votre profil</a>");
-                        }
-                    ?>
-                    <a href="repertoire_botannique.php">Le répertoire botannique</a>
-                    <a href="repertoire_utilisateur.php">Les utilisateurs</a>
-                    <a href="listeSignalement.php">Les derniers signalements</a>
-                    <a href="ajout_signalement.php">Signaler une plante</a>
-                    <?php 
-                        if($_SESSION['rang']==3){
-                            echo("<a href=\"ajout_plante.php\">Ajouter une plante</a>");
-                        }
-                    ?>
-                    
-                    <a href="">Vos amis</a>
-                    <?php
-                        if(isset($_SESSION['id']) and empty($_GET['deco'])){
-                            echo ("<a href=\"accueil.php?deco=1\">Déconnexion</a>");
-                        }
-                        else{
-                            echo("<a href=\"connexion.php\">Connexion</a> <a href=\"inscription.php\">Inscription</a>");
-                        }
-                    ?>
-                    
-                    
-                </div>
-</div>
-<?php
-   if($_GET['deco']==1){
-        session_destroy();
-        echo ("<p align=\"center\"> Vous avez été déconnecté </p>");
-    }
-
-?>
+    
+    <?php
+    include("menu.php");
+    ?>
 
 
 
