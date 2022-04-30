2fbfa2c232186702edd7da469da03d701ad3f2a4
MarouenKadri <95723839+MarouenKadri@users.noreply.github.com>
2022-04-30 20:50:38 +0200

    Add files via upload

--- FRONT_END/ajout_plante.php
+++ FRONT_END/ajout_plante.php
@@ -12,7 +12,12 @@
       </title>
       <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />  
       <link rel="stylesheet" type="text/css" href="css/my-login.css"> 
-      <link href ="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+      <link href ="css/bootstrap.css" rel="stylesheet" type="text/css"/>    
+
+      <script src="https://kit.fontawesome.com/6b6c1dbe0e.js" crossorigin="anonymous"></script>    
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"   
+integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">  
+<link rel="stylesheet" href="pageaccueil.css">
   </head>
 
   <body class="my-login-page">
@@ -20,184 +25,44 @@
 
 
 
+        
 
+ <header >
+                <div class="jumbotron   bg-image  text-Light    " >  
+                    <h1 style="color:white;text-align:center" >
+                        Plate-forme collaborative de lutte contre les plantes invasives
+                    </h1>    
+                    
+                </div>
+  </header>
+<!--$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ -->
 
-  <!-- Code PHP -->  
-  <?php
-   if($_GET['deco']==1){
-        session_destroy();
-        echo ("<p align=\"center\"> Vous avez été déconnecté </p>");
-    }
+<main>
 
+ 
+
+<h2 class="card-title" style="text-align:center" >   Ajouter une plante     </h2>  
+<hr>
+<?php
+include("menu.php");
 ?>
 
-    <?php
-      if(isset($_POST['valider'])){
-        if(!empty($_POST)){
 
-          $errors = array();
-
-          if(empty($_POST['nomfr']) || !preg_match('/^[A-Za-z ]+$/', $_POST['nomfr'])){
-            $errors['nomfr']="Nom français non valide";
-          }
-
-          if(empty($_POST['nomlat']) || !preg_match('/^[A-Za-z ]+$/', $_POST['nomlat'])){
-            $errors['nomlat']="Nom latin non valide";
-          }
-
-          if(empty($_POST['region']) || !preg_match('/^[A-Za-z ]+$/', $_POST['region'])){
-            $errors['region']="Région non valide";
-          }
-
-          if(empty($_POST['taille']) || !preg_match('/^[0-9 ]+$/', $_POST['taille'])){
-            $errors['taille']="Taille non valide";
-          }
-
-          if($_POST['famille']==NULL){
-            $errors['famille']="Famille botanique non remplie";
-          }
-
-          if(empty($_POST['couleur'])){
-            $errors['couleur']="Couleur non remplie";
-          }
-
-          if($_POST['fleur'] =='oui'){
-
-            if(empty($_POST['couleurfleur'])){
-              $errors['couleurfleur']="Couleur des fleurs non remplie";
-            }
-            if(empty($_POST['periodefleur'])){
-              $errors['periodefleur']="Période de floraison non remplie";
-            }
-
-          }
-
-          if($_POST['fruit'] =='oui'){
-
-            if(empty($_POST['couleurfruit'])){
-              $errors['couleurfruit']="Couleur des fruits non remplie";
-            }
-            if(empty($_POST['periodefruit'])){
-              $errors['periodefruit']="Période de fructification non remplie";
-            }
-
-          }
-
-          if(empty($_POST['famille'])){
-            $errors['famille']="Famille botanique non remplie";
-          }
-          
-           //connexion bdd           
-          try{
-            $BDD = new PDO('mysql:host=localhost;port=3308;dbname=bdd;charset=utf8', 'root', 'root');
-            $BDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $BDD->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-          }
-          catch(Exception $e){
-            echo 'Erreur :' . $e->getMessage();
-          }
-
-          #on vérifie si l'adresse mail est déjà dans la bdd  
-          $requetenom = $BDD->prepare('SELECT * FROM plantes WHERE Nom_fr ="'.ucfirst($_POST['nomfr']).'" OR Nom_latin ="'.ucfirst($_POST['nomlat']).'"');
-          $requetenom->execute();
-          $nb = $requetenom->rowCount();
-        
-          if($nb != 0)
-          {
-            $errors['nomexiste']="La plante est déjà dans la base de données";
-          }
-
-        }
-
-        if(empty($errors)){
-          
-          //récupération de toutes les valeurs
-          $nomfr = ucfirst($_POST['nomfr']);
-          $nomlat = ucfirst($_POST['nomlat']);
-          $famille = $_POST['famille'];
-          $region = $_POST['region'];
-          $taille = floatval($_POST['taille']);
-          $description = $_POST['description'];
-
-          $couleur=$_POST['couleur'];
-          
-          
-         if($_POST['fleur'] =='oui')
-          {
-            $fleur = 1;
-            $periodefleur = $_POST['periodefleur'];
-            $couleurfleur = $_POST['couleurfleur'];
-            
-          }
-          else
-          {
-            $fleur = 0;
-            $couleurfleur = NULL;
-            $periodefleur = NULL;
-          }
-
-          if($_POST['fruit'] =='oui')
-          {
-            $fruit = 1;
-            $couleurfruit = $_POST['couleurfruit'];
-            $periodefruit = $_POST['periodefruit'];;
-          }
-          else
-          {
-            $fruit = 0;
-            $couleurfruit = NULL;
-            $periodefruit = NULL;
-          }
-
-          //insertion
-         try{
-            $BDD = new PDO('mysql:host=localhost;port=3308;dbname=bdd;charset=utf8', 'root', 'root');
-            $BDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $BDD->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-          }
-          catch(Exception $e){
-            echo 'Erreur :' . $e->getMessage();
-          }
-          try{
-            $req = $BDD->prepare("INSERT INTO plantes (Id_utilisateur, Nom_latin, Nom_fr, Taille, Couleur, Fleur, Fruit, Couleur_fleur, Couleur_fruit, Régions, Details, Famille, Période_floraison, Période_fructification )  VALUES (:utilisateur, :nomlat, :nomfr, :taille, :couleur, :fleur, :fruit, :couleur_fleur, :couleur_fruit, :region, :details, :famille, :periode_floraison, :periode_fructification) ");
-            $exec = $req->execute(array(':utilisateur'=>$_SESSION['id'], ':nomlat'=> $nomlat, ':nomfr'=> $nomfr, ':taille'=> $taille, ':couleur'=> $couleur, ':fleur'=> $fleur, ':fruit'=> $fruit, ':couleur_fleur'=> $couleurfleur, ':couleur_fruit'=> $couleurfruit, ':region'=>$region, ':details'=> $description, ':famille'=> $famille, ':periode_floraison'=> $periodefleur, ':periode_fructification'=> $periodefruit));
-          }
-          catch(Exception $e){
-              echo "erreur".$e->getMessage();
-          }
-
-          header('Location: repertoire_botannique.php?ajout=true');
-          exit();
-        }
-      }
-    ?>  
+<div class="container">
+<div class="row ">  
 
 
+       
+ <div class="col">  
+                
+ 
 
-
-
-
-
-
-
-
-
-
-
-
-  
-  <section class="h-100 w-100">
-  <div class="container h-100 w-100">  
-  
-  <div class="d-flex flex-nowrap ">  
-  <div class="card-wrapper w-100 h-75">
-   <!--#################################################################################################################-->          
-   <div class="brand">
-				<img src="img/logo.png" alt="bootstrap 4 login page">
-		</div>
+<!--$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ -->                      
+<div class="brand">
+	<img src="img/logo.png" alt="bootstrap 4 login page">
+</div>
              
-    <div class="card fat">     
-      <div class="card-body">
+      
 
       <?php
       	if(!empty($errors)){
@@ -217,13 +82,13 @@
 <form method="post">
  <!-- Nom Français -->  
  
-    <div class="form-group">
+<div class="form-group">
 		<label for="email">Nom français : </label>
 		<input id="nom_fr" type="text" class="form-control" name="nomfr" required>
 		<div class="invalid-feedback">
         		Le nom français n'est pas valide
 		</div>
-		</div>
+</div>
 <!-- Nom Latin -->    
 
 <div class="form-group">
@@ -232,7 +97,7 @@
          <div class="invalid-feedback">
          	Le nom latin n'est pas valide
          </div>  
-     </div>
+ </div>
     
 
 <!--Famille botanique--> 
@@ -242,9 +107,9 @@
     </div>
     <select class="custom-select" id="inputGroupSelect01" name="famille" >
       
-      <option selected value="arbre">Arbre</option>
-      <option value="arbuste">Arbuste</option>
-      <option value="plante">Plante</option>
+        <option selected value="arbre">Arbre</option>
+        <option value="arbuste">Arbuste</option>
+        <option value="plante">Plante</option>
     </select>
   </div>
 <!-- Régions principales -->  
@@ -255,7 +120,7 @@
       <div class="invalid-feedback">
       Votre région  n'est pas valide
       </div>
-      </div>
+     </div>
 
 <!-- Couleur -->
   <div class="input-group mb-3">
@@ -325,16 +190,16 @@
 </div>     
 <!--Période de floraison -->    
 <div class="input-group mb-3">
-  <div class="input-group-prepend">
-    <label class="input-group-text" for="inputGroupSelect01">Période de floraison</label>
-  </div>
-  <select class="custom-select" id="inputGroupSelect01" name="periodefleur">
-    <option selected>Choose...</option>
-    <option value="printemps" selected>Printemps </option>
-    <option value="ete">Eté </option>
-    <option value="automne">Automne </option>
-    <option value="hiver">Hiver </option>
-  </select>
+     <div class="input-group-prepend">
+            <label class="input-group-text" for="inputGroupSelect01">Période de floraison</label>
+     </div>
+     <select class="custom-select" id="inputGroupSelect01" name="periodefleur">
+             <option selected>Choose...</option>
+             <option value="printemps" selected>Printemps </option>
+             <option value="ete">Eté </option>
+             <option value="automne">Automne </option>
+             <option value="hiver">Hiver </option>
+    </select>
 </div>
 
 <!-- Posséde de fruit -->
@@ -357,8 +222,7 @@
         
 
 </div>
-        </div>
-      </div>    
+         
 <!-- Couleur fruit -->
     
 <div class="input-group mb-3">
@@ -383,7 +247,7 @@
    
   <!--  Période de fructification-->
 
-    <div class="input-group mb-3">
+<div class="input-group mb-3">
       <div class="input-group-prepend">
         <label class="input-group-text" for="inputGroupSelect01">Période de fructification</label>
       </div>
@@ -393,8 +257,8 @@
         <option value="ete">Eté </option>
         <option value="automne">Automne </option>
         <option value="hiver">Hiver </option>
-      </select>
-    </div>
+    </select>
+</div>
 
 <!-- Decription -->   
   <div class="form-group">
@@ -403,31 +267,28 @@
       <div class="invalid-feedback">
                               
       </div>
-      </div>
+  </div>
 
 <!-- Ajout une photo -->    
 <div class="form-group">
-<label for="formFileMultiple" class="form-label">Ajouter photos</label>
-<input class="form-control"  type="file" id="formFileMultiple" multiple />
+    <label for="formFileMultiple" class="form-label">Ajouter photos</label>
+    <input class="form-control"  type="file" id="formFileMultiple" multiple />
 
 </div>
 
 
-   
-   <div class="form-group m-0">
+<div class="form-group m-0">
     <button type="submit" class="btn btn-primary btn-block" name="valider">
           Valider
     </button>
     <button type="submit" class="btn btn-primary btn-block">
       Annuler
-</button>
+    </button>
     
-     
-              
-  </div> 
+</div> 
 
 
-    </form>
+</form>
 
 
    <div class="footer">
@@ -435,14 +296,77 @@
   </div>
 
 </div>
-  </div>
-   </div>   
-  </div> 
-  </section>
+
+
+ 
+                       
+
+                   
+
+
+                 
+        
+               
+
+                
+
+
+
+
+<!--$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ -->
+     </div>
+     </div>
+              
+    </div>
+     </div>
+    </div>
+
+
+    
+</main>
+
+   
+
+
+
+
+
+
+ 
+
+  
+   
+        
+        
+          
+           
+            
+          
+
+       
+
+
+  
+
+
   <script src="js/jquery-3.6.0.min.js"></script>  
   <script src="js/popper.min.js"></script>  
   <script src="js/bootstrap.min.js"></script>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
-
