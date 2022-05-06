eccf32ce1d23445059cc09bdb95d53e5325bb0cc
MarouenKadri <95723839+MarouenKadri@users.noreply.github.com>
2022-05-07 01:37:17 +0200

    Update listeSignalement.php

--- FRONT_END/listeSignalement.php
+++ FRONT_END/listeSignalement.php
@@ -49,7 +49,7 @@
      <div class="card-body">   
 
      <div class="input-group">
-    <input type="search" class="form-control rounded" id="searchbar" name="plante" placeholder="Rechercher une plante..." aria-label="Search" aria-describedby="search-addon" />
+    <input type="search" class="form-control rounded" id="searchbar" name="plante" placeholder="Rechercher un signalement..." aria-label="Search" aria-describedby="search-addon" />
      <button type="submit" class="btn btn-outline-primary" name="searchbar">Rechercher</button>
     </div>
           
